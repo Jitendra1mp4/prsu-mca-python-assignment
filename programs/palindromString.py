182144dedc484edda02fe8f1ccf202d33b9c1daf
Jitendra Kumar <jitendrasahu6761@gmail.com>
2024-07-14 13:17:16 +0530

    update program shorter check for palindrom using slicing operator

--- programs/palindromString.py
+++ programs/palindromString.py
@@ -1,12 +1,7 @@
 #  check and print if entered string is palindrome or not
 import introJitendra
 introJitendra.printIntro("check and print if entered string is palindrom or not")
-
 s=input("enter the string to be checked : ") ;
-isPalindrome=True 
-for i in range(len(s)//2):
-    if s[i] != s[-(i+1)] :
-        print("no palindrome")
-        isPalindrome=False ;
-        break ;
+isPalindrome= s==s[::-1]
 if isPalindrome : print("Palindrome")
+else :   print("Not palindrome")
